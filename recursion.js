81c48085d43bba49ae80b586eb59b5bc486039fa
Dmitry Avilov <avilov.d@fmedia.tech>
2022-03-30 17:02:49 +0300

    fix: RecursionDNR: Fixed proto array destroy

--- recursion.js
+++ recursion.js
@@ -3,16 +3,18 @@ const arr = [5, 10, 20, 8];
 function getSum(array) {
     const last = array[array.length - 1];
     let sum = 0;
+    array = [...array];
     
     if (array.length === 1) {
         return arr[0];
     }
 
     array.splice(-1, 1);
-    
+
     sum = getSum(array) + last;
 
     return sum;
 };
 
 console.log(getSum(arr));
+console.log(arr);
