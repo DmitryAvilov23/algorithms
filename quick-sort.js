867737edb4857f4eada6968440872fb7d1e9f2ab
Dmitry Avilov <avilov.d@fmedia.tech>
2022-03-31 14:48:22 +0300

    feat: QuickSort: Completed

---
+++ quick-sort.js
@@ -0,0 +1,20 @@
+const arr3 = [1, 8, 5, 6, 2, 4, 3, 2, 2, 2, 10, -3];
+
+function quickSort(array) {
+    if (array.length < 2) {
+        return array;
+    };
+
+    const pivot = array[0];
+    const less = [];
+    const greater = [];
+
+    array.forEach(item => {
+        if (item < pivot) less.push(item);
+        if (item > pivot) greater.push(item);
+    });
+
+    return [...quickSort(less), pivot, ...quickSort(greater)];
+};
+
+console.log(quickSort(arr3));
